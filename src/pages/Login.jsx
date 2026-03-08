0f0db4b9b1234e0af4b79b8bf65b21de45e8791f
SriLathvikaGundam <gundamsrilathvika@gmail.com>
2026-03-08 22:19:38 +0530

    add login redirect

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (email && password) {
+      alert("Login Successful!");
+
+      // redirect to biodata builder page
+      navigate("/builder");
+    } else {
+      alert("Please enter email and password");
+    }
+  };
+
   return (
     <div className="auth-container">
       <h2>ShaadiBio Login</h2>
 
-      <input type="email" placeholder="Email" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-      <button>Login</button>
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
 
       <p>
         Don't have an account? <Link to="/register">Register</Link>
